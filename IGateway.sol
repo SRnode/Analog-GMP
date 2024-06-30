56e30d1420ead6fa7b6d58ab6b0fca068a2bd5f5
SrNode <146883132+SRnode@users.noreply.github.com>
2024-06-30 21:23:46 +0700

    Update IGateway.sol

--- IGateway.sol
+++ IGateway.sol
@@ -1,140 +1,63 @@
 // SPDX-License-Identifier: MIT
-// Analog's Contracts (last updated v0.1.0) (src/utils/BranchlessMath.sol)
+// Analog's Contracts (last updated v0.1.0) (src/interfaces/IGateway.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.0;
+
+import {GmpSender} from "contracts/Primitives.sol";
 
 /**
- * @dev Utilities for branchless operations, useful when a constant gas cost is required.
+ * @dev Required interface of an Gateway compliant contract
  */
-library BranchlessMath {
+interface IGateway {
     /**
-     * @dev Returns the smallest of two numbers.
+     * @dev New GMP submitted by calling the `submitMessage` method.
+     * @param id EIP-712 hash of the `GmpPayload`, which is it's unique identifier
+     * @param source sender account, with an extra flag indicating if it is a contract or an EOA
+     * @param destinationAddress the target address on the destination chain.
+     * @param destinationNetwork the target chain where the contract call will be made.
+     * @param executionGasLimit the gas limit available for the contract call
+     * @param salt salt is equal to the previous message id (EIP-712 hash).
+     * @param data message data with no specified format
      */
-    function min(uint256 x, uint256 y) internal pure returns (uint256) {
-        return select(x < y, x, y);
-    }
+    event GmpCreated(
+        bytes32 indexed id,
+        bytes32 indexed source,
+        address indexed destinationAddress,
+        uint16 destinationNetwork,
+        uint256 executionGasLimit,
+        uint256 salt,
+        bytes data
+    );
+
+    function networkId() external view returns (uint16);
 
     /**
-     * @dev Returns the largest of two numbers.
+     * @notice Pay for gas of contract execution on destination chain.
+     * @dev This function is called on the destination chain before calling the gateway to execute a source contract.
+     * @param sender The address making the payment
+     * @param sourceNetwork The target chain where the contract call will be made
      */
-    function max(uint256 x, uint256 y) internal pure returns (uint256) {
-        return select(x > y, x, y);
-    }
+    function deposit(GmpSender sender, uint16 sourceNetwork) external payable;
 
     /**
-     * @dev If `condition` is true returns `a`, otherwise returns `b`.
+     * @notice Pay for gas of contract execution on destination chain.
+     * @dev This function is called on the destination chain before calling the gateway to execute a source contract.
+     * @param sender The address making the payment
+     * @param sourceNetwork The target chain where the contract call will be made
      */
-    function select(bool condition, uint256 a, uint256 b) internal pure returns (uint256) {
-        unchecked {
-            // branchless select, works because:
-            // b ^ (a ^ b) == a
-            // b ^ 0 == b
-            //
-            // This is better than doing `condition ? a : b` because:
-            // - Consumes less gas
-            // - Constant gas cost regardless the inputs
-            // - Reduces the final bytecode size
-            return b ^ ((a ^ b) * toUint(condition));
-        }
-    }
+    function depositOf(GmpSender sender, uint16 sourceNetwork) external view returns (uint256);
 
     /**
-     * @dev If `condition` is true returns `a`, otherwise returns `b`.
+     * @dev Send message from chain A to chain B
+     * @param destinationAddress the target address on the destination chain
+     * @param destinationNetwork the target chain where the contract call will be made
+     * @param executionGasLimit the gas limit available for the contract call
+     * @param data message data with no specified format
      */
-    function select(bool condition, address a, address b) internal pure returns (address) {
-        return address(uint160(select(condition, uint256(uint160(a)), uint256(uint160(b)))));
-    }
-
-    /**
-     * @dev If `condition` is true return `value`, otherwise return zero.
-     */
-    function selectIf(bool condition, uint256 value) internal pure returns (uint256) {
-        unchecked {
-            return value * toUint(condition);
-        }
-    }
-
-    /**
-     * @dev Unsigned saturating addition, bounds to UINT256 MAX instead of overflowing.
-     * equivalent to:
-     * uint256 r = x + y;
-     * return r >= x ? r : UINT256_MAX;
-     */
-    function saturatingAdd(uint256 x, uint256 y) internal pure returns (uint256) {
-        unchecked {
-            x = x + y;
-            y = 0 - toUint(x < y);
-            return x | y;
-        }
-    }
-
-    /**
-     * @dev Unsigned saturating subtraction, bounds to zero instead of overflowing.
-     * equivalent to: x > y ? x - y : 0
-     */
-    function saturatingSub(uint256 a, uint256 b) internal pure returns (uint256) {
-        unchecked {
-            // equivalent to: a > b ? a - b : 0
-            return (a - b) * toUint(a > b);
-        }
-    }
-
-    /**
-     * @dev Unsigned saturating multiplication, bounds to `2 ** 256 - 1` instead of overflowing.
-     */
-    function saturatingMul(uint256 a, uint256 b) internal pure returns (uint256) {
-        unchecked {
-            uint256 c = a * b;
-            bool success;
-            assembly {
-                // Only true when the multiplication doesn't overflow
-                // (c / a == b) || (a == 0)
-                success := or(eq(div(c, a), b), iszero(a))
-            }
-            return c | (toUint(success) - 1);
-        }
-    }
-
-    /**
-     * @dev Unsigned saturating division, bounds to UINT256 MAX instead of overflowing.
-     */
-    function saturatingDiv(uint256 x, uint256 y) internal pure returns (uint256 r) {
-        assembly {
-            r := div(x, y)
-        }
-    }
-
-    /**
-     * @dev Returns the ceiling of the division of two numbers.
-     *
-     * This differs from standard division with `/` in that it rounds towards infinity instead
-     * of rounding towards zero.
-     */
-    function ceilDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-        unchecked {
-            // The following calculation ensures accurate ceiling division without overflow.
-            // Since a is non-zero, (a - 1) / b will not overflow.
-            // The largest possible result occurs when (a - 1) / b is type(uint256).max,
-            // but the largest value we can obtain is type(uint256).max - 1, which happens
-            // when a = type(uint256).max and b = 1.
-            return selectIf(a > 0, ((a - 1) / b + 1));
-        }
-    }
-
-    /**
-     * @dev Cast a boolean (false or true) to a uint256 (0 or 1) with no jump.
-     */
-    function toUint(bool b) internal pure returns (uint256 u) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            u := iszero(iszero(b))
-        }
-    }
-
-    /**
-     * @dev Cast an address to uint256
-     */
-    function toUint(address addr) internal pure returns (uint256) {
-        return uint256(uint160(addr));
-    }
+    function submitMessage(
+        address destinationAddress,
+        uint16 destinationNetwork,
+        uint256 executionGasLimit,
+        bytes calldata data
+    ) external payable returns (bytes32);
 }
